d71bc2618395963fbe8d2fcb53433549a23cd147
Jordy Delgado <jordydelgado@hotmail.com>
2023-08-30 10:10:50 -0500

    Gateway setup working

---
+++ gateway/src/constants/index.ts
@@ -0,0 +1,4 @@
+export const SERVICE_BASE_PATH = 'http://localhost';
+export const USER_SERVICE_PORT = 3105;
+export const GATEWAY_PORT = 223;
+export const GRAPHQL_URL_COMPLEMENT = '/graphql';
--- users-service/src/main.ts
+++ users-service/src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const PORT = process.env.USER_SERVICE_PORT;
+const port = 3105;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(PORT);
+  console.log(`server ready to listen at port ${port}`);
+  await app.listen(port);
 }
 
 bootstrap();
